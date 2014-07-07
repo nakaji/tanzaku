f2781971125a484ac731d55077d4dcd8ca14f23e
nakaji <nakaji126@gmail.com>
2014-07-08 00:19:57 +0900

    WebAPIを追加

---
+++ Tanzaku/Controllers/TanzakuController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Query.Dynamic;
+using Tanzaku.Models;
+
+namespace Tanzaku.Controllers
+{
+    public class TanzakuController : ApiController
+    {
+        public dynamic Get(string text="")
+        {
+            var generator = new TanzakuMaker();
+
+            return Json(new { Text = generator.Generate(text)});
+        }
+    }
+}
