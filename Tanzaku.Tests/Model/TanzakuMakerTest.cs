ae63a09847f1e621087ff656d6586d7a9b777bbf
nakaji <nakaji126@gmail.com>
2014-07-07 23:42:15 +0900

    モデルを追加

---
+++ Tanzaku.Tests/Model/TanzakuMakerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tanzaku.Models;
+
+namespace Tanzaku.Tests.Model
+{
+    [TestClass]
+    public class TanzakuMakerTest
+    {
+        private TanzakuMaker _sut = new TanzakuMaker();
+
+        [TestMethod]
+        public void 何も入力がないときは枠だけ表示する()
+        {
+            var actual = _sut.Generate("");
+
+            Assert.AreEqual(@"┏┻┓
+┗━┛
+", actual);
+        }
+
+        [TestMethod]
+        public void 入力文字を縦書きで表示する()
+        {
+            var actual = _sut.Generate("テスト");
+
+            Assert.AreEqual(@"┏┻┓
+┃テ┃
+┃ス┃
+┃ト┃
+┗━┛
+", actual);
+        }
+    }
+}
---
+++ Tanzaku/Models/TanzakuMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using WebGrease.Css.Extensions;
+
+namespace Tanzaku.Models
+{
+    public class TanzakuMaker
+    {
+        public string Generate(string text)
+        {
+            var sb=new StringBuilder();
+            sb.AppendLine("┏┻┓");
+
+            var vartString = new Regex("(.)").Replace(text, "┃$1┃\r\n");
+            sb.Append(vartString);
+
+            sb.AppendLine("┗━┛");
+            return sb.ToString();
+        }
+    }
+}
